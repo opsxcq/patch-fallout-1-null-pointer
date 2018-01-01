f1866e6df1a50a419a13cfb32a9e96125e43c852
OPSXCQ <opsxcq@strm.sh>
2018-01-01 00:18:23 +0000

    git ignore added

---
+++ generate-source.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### Generate C source for .patch files
+
+if [ -z "$1" ]
+then
+    echo '[-] Please inform a patch file to be transformed'
+    exit -1
+fi
+
+for line in $(cat $1 | grep ':')
+do
+    byteOffset=$(echo $line | cut -d ':' -f 1)
+    byteValue=$(echo $line | cut -d '>' -f 2 | tr -d '\n')
+    echo '  fseek(gameFile, 0, 0x'${byteOffset}');'
+    echo  "  fwrite((void *) \"\x$byteValue\", 1, 1, gameFile);"
+done
